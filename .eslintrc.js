3b3608d1f69c2dc0347848e01b47fa433f4b6066
xjiaxiang <xjiaxiang_86@163.com>
2023-02-17 15:18:30 +0800

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
