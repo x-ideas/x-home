9e4213254b8d29124d404abdfc241ccb7273611e
xjiaxiang <xjiaxiang_86@163.com>
2024-01-06 10:04:07 +0800

    feat(home): 更新首页

--- packages/home-index/src/pages/index.tsx
+++ packages/home-index/src/pages/index.tsx
@@ -1,11 +1,41 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import FilterButtons from "./filter-buttons";
+import ProjectList from "./project-list";
+import { useState } from "react";
+import { uniq } from "lodash-es";
 
-const inter = Inter({ subsets: ["latin"] });
+interface ProjectInfo {
+  value: string;
+  name: string;
+  /** 类别 */
+  category: string;
+}
 
 export default function Home() {
+  const [selectedCategory, setSelectedCategory] = useState<string>("All");
+  const [projects] = useState<ProjectInfo[]>([
+    {
+      value: "orizon",
+      name: "Orizon",
+      category: "web development",
+    },
+    {
+      value: "orizon",
+      name: "Orizon",
+      category: "web design",
+    },
+    {
+      value: "orizon",
+      name: "Orizon",
+      category: "applications",
+    },
+  ]);
+
+  const categories = Object.values(projects).map((proj) => proj.category);
+  categories.unshift("All");
+  const allCategories = uniq(categories);
+
   return (
     <>
       <Head>
@@ -15,108 +45,36 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+        <article className="portfolio active" data-page="portfolio">
+          <header>
+            <h2 className="h2 article-title">XXX</h2>
+          </header>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+          <section className="projects">
+            <FilterButtons
+              value={selectedCategory}
+              options={allCategories.map((category) => {
+                return {
+                  value: category,
+                  name: category,
+                };
+              })}
+              onChange={(newValue) => {
+                setSelectedCategory(newValue);
+              }}
+            ></FilterButtons>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+            <ProjectList
+              allOptions={projects}
+              filter={(opt) => {
+                if (selectedCategory === "All") {
+                  return true;
+                }
+                return opt.category === selectedCategory;
+              }}
+            ></ProjectList>
+          </section>
+        </article>
       </main>
     </>
   );
