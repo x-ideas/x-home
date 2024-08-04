76f5008d9e2bc3a492fda62e27ffda0ba087d2e5
xjiaxiang <xjiaxiang_86@163.com>
2024-08-04 19:19:07 +0800

    chore: 新增环境变量的类型定义
    说明:
    新增的定义用于supabase client

--- packages/home-index/next-env.d.ts
+++ packages/home-index/next-env.d.ts
@@ -3,3 +3,12 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    /** supabase rest api url */
+    readonly SUPABASE_REST_API_URL: string;
+    /** supabase anon key for rest api */
+    readonly SUPABASE_ANON_KEY: string;
+  }
+}
