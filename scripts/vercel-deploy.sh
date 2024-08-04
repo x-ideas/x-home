f692e79788e07475818b5bc6873019491bcf0531
xjiaxiang <xjiaxiang_86@163.com>
2024-08-05 07:59:25 +0800

    feat: 配置自定义vercel deploy的脚本
    说明
    只在main和develop分支下，才会执行vercel的depoly

---
+++ scripts/vercel-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+
+# only deploy if the branch is main or develop
+
+echo "当前分支：$VERCEL_GIT_COMMIT_REF"
+
+if [ "$VERCEL_GIT_COMMIT_REF" != "main" ] && [ "$VERCEL_GIT_COMMIT_REF" != "develop" ]; then
+  echo "Don't deploy"
+  exit 0
+fi
