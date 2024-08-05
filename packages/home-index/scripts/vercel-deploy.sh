2a8e5e404959d966c4f390cc7bbde43f4b0ff6f2
xjiaxiang <xjiaxiang_86@163.com>
2024-08-05 08:03:21 +0800

    chore: vercel deploy脚本新增else部分，让允许的分支能正常deploy

--- packages/home-index/scripts/vercel-deploy.sh
+++ packages/home-index/scripts/vercel-deploy.sh
@@ -9,4 +9,7 @@ echo "当前分支：$VERCEL_GIT_COMMIT_REF"
 if [ "$VERCEL_GIT_COMMIT_REF" != "main" ] && [ "$VERCEL_GIT_COMMIT_REF" != "develop" ]; then
   echo "Don't deploy"
   exit 0
+else
+  echo "Deploy"
+  exit 1
 fi
