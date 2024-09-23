82435ddfb9b930516cf9e072f523e4d6d226fc6a
xjiaxiang <xjiaxiang_86@163.com>
2024-09-23 20:51:14 +0800

    chore: remove old supabase client

--- packages/home-index/src/services-api/client.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-
-import { Database } from "./database.types.ts";
-
-export const supabase = createBrowserClient<Database>(
-  process.env.SUPABASE_REST_API_URL,
-  process.env.SUPABASE_ANON_KEY,
-);
--- packages/home-index/supabase/seed.sql
+++ packages/home-index/supabase/seed.sql
@@ -1,6 +1,6 @@
 -- in supabase/seed.sql
 INSERT INTO projects(name, description, remark, visibility)
-  VALUES ('x-blog', 'My blog', 'realtime comment, android app, pwa offline, auth, storage, 响应式布局，日志', 'Public');
+  VALUES ('x-blog', 'My blog', 'realtime comment, android app, pwa offline, auth, storage, 响应式布局，日志, RSS', 'Public');
 
 INSERT INTO projects(name, description, remark, visibility)
   VALUES ('x-world-3d', 'world 3d', 'threejs', 'Private');
