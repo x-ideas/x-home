a1654a17727a19af616d109a4b4ca97a17d30be4
xjiaxiang <xjiaxiang_86@163.com>
2024-09-23 20:03:13 +0800

    fix: remove old supabase client

--- packages/home-index/src/app/api/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-import { Database } from "./database.types.ts";
-
-export const supabase = createClient<Database>(
-  process.env.SUPABASE_REST_API_URL,
-  process.env.SUPABASE_ANON_KEY,
-);
--- packages/home-index/src/app/api/projects/route.ts
+++ packages/home-index/src/app/api/projects/route.ts
@@ -1,13 +1,13 @@
 import { type NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-import { supabase } from "../client";
+import { createClient } from "@xxx-utils/supabase/server";
 
 /**
  * 获取project list的接口实现
  */
 export async function GET(_request: NextRequest) {
-  const res = await supabase.from("projects").select();
+  const res = await createClient().from("projects").select();
 
   return NextResponse.json(res);
 }
