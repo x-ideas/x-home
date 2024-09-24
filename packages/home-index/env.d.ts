f3ccd12505833e55e3bafa8abf244f8dd58f108f
xjiaxiang <xjiaxiang_86@163.com>
2024-09-24 08:37:05 +0800

    feat: add pwa install feature

--- packages/home-index/env.d.ts
+++ packages/home-index/env.d.ts
@@ -4,5 +4,17 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_SUPABASE_URL: string;
     /** supabase anon key for rest api */
     readonly NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    /** web-push key相关 */
+    readonly NEXT_PUBLIC_VAPID_PUBLIC_KEY: string;
+    /** web-push key相关 */
+    readonly VAPID_PRIVATE_KEY: string;
+
+    /**
+     * 运行环境
+     * - dev: 本地开发环境, 在.env.local中配置
+     * - test: 测试环境，配置在vercel中
+     * - prod: 生产环境，配置在vercel中
+     */
+    readonly NEXT_PUBLIC_RUN_ENV: "dev" | "test" | "prod";
   }
 }
---
+++ packages/home-index/public/sw.js
@@ -0,0 +1,24 @@
+/* eslint-env worker */
+self.addEventListener("push", function (event) {
+  if (event.data) {
+    const data = event.data.json();
+    const options = {
+      body: data.body,
+      icon: data.icon || "/icon.png",
+      badge: "/badge.png",
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: "2",
+      },
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+  }
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("Notification click received.");
+  event.notification.close();
+  // eslint-disable-next-line no-undef
+  event.waitUntil(clients.openWindow("https://localhost:3000"));
+});
