1ad1f2682d8dff6f50f6933b06f80eca6b2a7004
xjiaxiang <xjiaxiang_86@163.com>
2024-09-26 08:19:59 +0800

    chore: add offline page

--- packages/home-index/next.config.mjs
+++ packages/home-index/next.config.mjs
@@ -8,7 +8,7 @@ const withSerwist = withSerwistInit({
   cacheOnNavigation: true,
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
-  additionalPrecacheEntries: [{ url: "/~offline", revision }],
+  additionalPrecacheEntries: [{ url: "/offline", revision }],
 });
 
 /** @type {import('next').NextConfig} */
